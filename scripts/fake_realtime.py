c47e5b6e9575ab201994271ea1a445ca3b80fc09
akrherz <akrherz@iastate.edu>
2013-02-12 07:59:37 -0600

    cleanups and improve time shifting logic

--- scripts/fake_realtime.py
+++ scripts/fake_realtime.py
@@ -1,37 +1,56 @@
-# Need something to drive the placement of files in a realtime directory
+"""
+ Need something to drive the placement of files in a realtime directory
+
+the times would be from 0423Z to 0737Z
+
+3hr14m (194m) in 90m
+
+KSGF20120229_030039_V06
+KSGF20120229_085728_V06
+"""
 
 import mx.DateTime
 import glob, os, time
 
-ABASE_TS = mx.DateTime.DateTime(2012,2,29,0,30)
-# 6 hours of data in 3 hours of time
-RT_START = mx.DateTime.DateTime(2012,12,15,18,10) # GMT!
-speedup = 2.0
+# First mesh point
+ARCHIVE_T0 = mx.DateTime.DateTime(2012,2,29,4,23)
+RT_T0 = mx.DateTime.DateTime(2013,2,12,20,30) # 2:30 PM
+# Second mesh point
+ARCHIVE_T1 = mx.DateTime.DateTime(2012,2,29,7,37)
+RT_T1 = RT_T0 + mx.DateTime.RelativeDateTime(minutes=90) 
+
+SPEEDUP = (ARCHIVE_T1 - ARCHIVE_T0).minutes / (RT_T1 - RT_T0).minutes
+print 'Speedup is %.2f' % (SPEEDUP,)
 
 def doit( fp, ts ):
-  cmd = "../l2munger KDMX %s %s" % (ts.strftime("%Y/%m/%d %H:%M:%S"), fp)
-  os.system(cmd)
-  fp = "KDMX%s" % (ts.strftime("%Y%m%d_%H%M%S"),)
-  os.system("compress %s" % (fp,))
-  os.rename("%s.Z" % (fp,), "../../htdocs/l2data/KDMX/%s.Z" % (fp,)) 
-  os.chdir("../../htdocs/l2data/KDMX/")
-  os.system("ls -l KDMX* | awk '{print $5 \" \" $9}' > dir.list")
-  os.chdir("../../../scripts/l2data")
+    cmd = "../l2munger KDMX %s %s" % (ts.strftime("%Y/%m/%d %H:%M:%S"), fp)
+    os.system(cmd)
+    fp = "KDMX%s" % (ts.strftime("%Y%m%d_%H%M%S"),)
+    os.system("compress %s" % (fp,))
+    os.rename("%s.Z" % (fp,), "../../htdocs/l2data/KDMX/%s.Z" % (fp,)) 
+    os.chdir("../../htdocs/l2data/KDMX/")
+    os.system("ls -l KDMX* | awk '{print $5 \" \" $9}' > dir.list")
+    os.chdir("../../../scripts/l2data")
 
-# Load Filenames, figure if they should be immediately moved
-os.chdir("l2data")
-files = glob.glob("*")
-files.sort()
-delayed = []
-left = len(files)
-for file in files:
-  # KTLX20090210_180130_V03
-  ts = mx.DateTime.strptime(file[4:19], '%Y%m%d_%H%M%S')
-  offset = (ts - ABASE_TS).minutes / speedup
-  fakets = RT_START + mx.DateTime.RelativeDateTime(minutes=offset)
-  print "Process: %s TS: %s Left: %s" % (file, 
-     fakets.strftime("%Y-%m-%d %H:%M"), left )
-  if fakets > mx.DateTime.gmt():
-    time.sleep( int((fakets - mx.DateTime.gmt()).seconds) )
-  left -= 1
-  doit( file, fakets )
+def main():
+    # Load Filenames, figure if they should be immediately moved
+    os.chdir("l2data")
+    files = glob.glob("*")
+    files.sort()
+    left = len(files)
+    for fn in files:
+        # KTLX20090210_180130_V03
+        ts = mx.DateTime.strptime(fn[4:19], '%Y%m%d_%H%M%S')
+      
+        offset = (ts - ARCHIVE_T0).minutes / SPEEDUP
+        fakets = RT_T0 + mx.DateTime.RelativeDateTime(minutes=offset)
+        print "Process: %s TS: %s Left: %s" % (fn, 
+                                fakets.strftime("%Y-%m-%d %H:%M"), left )
+        if fakets > mx.DateTime.gmt():
+            secs = int((fakets - mx.DateTime.gmt()).seconds)
+            print 'Sleeping for %s seconds' % (secs,)
+            time.sleep( int((fakets - mx.DateTime.gmt()).seconds) )
+        left -= 1
+        doit( fn, fakets )
+
+main()
