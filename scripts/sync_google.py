cd4616d4ae24c890b59eef5c2bbd55cbab182207
akrherz <akrherz@iastate.edu>
2019-03-25 09:29:40 -0500

    update to use new google sheets client

--- scripts/sync_google.py
+++ scripts/sync_google.py
@@ -7,6 +7,22 @@ import pytz
 import pyiem.cscap_utils as util
 
 SHEET = "1tkIttjziC4Ob3Gh9oWXuFs1-sE0CUNfIzpw_vjfSxek"
+LKP = {
+    'HAIL': 'H',
+    'TORNADO': 'T',
+    'TSTM WND GST': 'G',
+    'WIND': 'G',
+    'TSTM WND DMG': 'D',
+    'NON-TSTM WND DMG': 'O',
+    'NON-TSTM WND GST': 'N',
+    'FUNNEL CLOUD': 'C',
+    'HEAVY RAIN': 'R',
+    'FLASH FLOOD': 'F',
+    'FLOOD': 'F',
+    'WALL CLOUD': 'X',
+    'LIGHTNING': 'L'
+}
+
 
 def convtime(val):
     """Convert time"""
@@ -26,55 +42,49 @@ def main():
     print('Deleted %s rows' % (mcursor.rowcount,))
 
     # Get me a client, stat
-    spr_client = util.get_spreadsheet_client(util.get_config())
-
-    feed = spr_client.get_list_feed(SHEET, "od6")
-
-    lkp = {'HAIL': 'H',
-           'TORNADO': 'T',
-           'TSTM WND GST': 'G',
-           'WIND': 'G',
-           'TSTM WND DMG': 'D',
-           'TSTM WND DMG': 'D',
-           'NON-TSTM WND DMG': 'O',
-           'NON-TSTM WND GST': 'N',
-           'FUNNEL CLOUD': 'C',
-           'HEAVY RAIN': 'R',
-           'FLASH FLOOD': 'F',
-           'FLOOD': 'F',
-           'WALL CLOUD': 'X',
-           'LIGHTNING': 'L'}
+    config = util.get_config()
+    sheets = util.get_sheetsclient(config, "cscap")
+    f = sheets.spreadsheets().get(
+        spreadsheetId=SHEET, includeGridData=True
+    )
+    j = util.exponential_backoff(f.execute)
 
     inserts = 0
-    for entry in feed.entry:
-        data = entry.to_dict()
-        ts = convtime(data['workshoputc'])
-        ts = ts.replace(tzinfo=pytz.utc)
-        if data['workshoprevealutc'] is None:
+    grid = j['sheets'][0]['data'][0]
+    cols = [a['formattedValue'] for a in grid['rowData'][0]['values']]
+    print(cols)
+    for row in grid['rowData'][1:]:
+        vals = [a.get('formattedValue') for a in row['values']]
+        data = dict(zip(cols, vals))
+        if data['Workshop UTC'] is None:
+            continue
+        ts = convtime(data['Workshop UTC'])
+        ts = ts.replace(tzinfo=pytz.UTC)
+        if data['Workshop Reveal UTC'] is None:
             revealts = ts
         else:
-            revealts = convtime(data['workshoprevealutc'])
-            revealts = revealts.replace(tzinfo=pytz.utc)
+            revealts = convtime(data['Workshop Reveal UTC'])
+            revealts = revealts.replace(tzinfo=pytz.UTC)
         if ts != revealts:
             print(("  Entry has reveal delta of %s minutes"
-                ) % ((revealts - ts).total_seconds() / 60.,))
-        geo = 'SRID=4326;POINT(%s %s)' % (data['lon'], data['lat'])
+                   ) % ((revealts - ts).total_seconds() / 60.,))
+        geo = 'SRID=4326;POINT(%s %s)' % (data['LON'], data['LAT'])
         sql = """
         INSERT into lsrs (valid, display_valid, type, magnitude, city, source,
         remark, typetext, geom, wfo) values (%s, %s, %s, %s, %s, %s,
         %s, %s, %s, 'DMX')"""
         args = (ts, revealts,
-                lkp[data['type']],
-                0 if data['magnitude'] == 'None' else data['magnitude'],
-                data['workshopcity'], data['source'], data['remark'],
-                data['type'], geo)
+                LKP[data['Type']],
+                0 if data['Magnitude'] == 'None' else data['Magnitude'],
+                data['Workshop City'], data['source'], data['Remark'],
+                data['Type'], geo)
         mcursor.execute(sql, args)
         inserts += 1
 
     mcursor.close()
     mydb.commit()
     print("Inserted %s new entries!" % (inserts,))
-    print(("ALERT: Consider running assign_lsr_wfo.py to get WFO right in DB"))
+    print("ALERT: Consider running assign_lsr_wfo.py to get WFO right in DB")
 
 
 if __name__ == '__main__':
